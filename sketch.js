f6a5f54ba1899f0bac908731122d0dc3a3cd42cf
Indiae Gill <igill@wlapcs.org>
2017-03-28 17:41:42 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas(600,400);
+    background(137,30,100);
+}
+
+function draw(){
+    stroke(255,255,255);
+    line(35,80,300,200);
+    line(35,80,300,200);
+    
+    stroke(14,214,174);
+    fill(255,76,0);
+    rect(290,100,250,250);
+    rect(200,100,20,75);
+     line(-35,-80,-300,-200);
+    stroke(239,255,17);
+    strokeWeight(5);
+    fill(20,3,255);
+    ellipse(90,250,60,80);
+    ellipse(40,40,30,30);
+    
+    line(100,360,450,110);
+    line(100,160,100,80);
+    line(250,300,125,300)
+    
+}     
